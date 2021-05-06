63b01aa2892add67c57e6cd96249400672e3d52d
Adam Tomczyk <markus.adam.tomczyk@gmail.com>
2021-05-06 17:48:27 +0200

    Set up of structure and vars as well as first while loop for input

---
+++ index.js
@@ -0,0 +1,9 @@
+let input = prompt("What would you like to do?")
+
+let quittingCommand = "".toLowerCase();
+
+while(quittingCommand !== "quit"){
+    input =  prompt("What would you like to do?")
+}
+
+alert("Okay, you quit the input")

58060d1f1854622a7c40c56f2b2bf5441c4d138a
Adam Tomczyk <markus.adam.tomczyk@gmail.com>
2021-05-06 18:53:31 +0200

    Basic funcionality done

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-let input = prompt("What would you like to do?")
+// Input Var
+let input = prompt('What would you like to do?')
 
-let quittingCommand = "".toLowerCase();
 
-while(quittingCommand !== "quit"){
+// Todos Array
+const todos = [];
+
+while(input !== 'quit'){
+    if(input === 'list'){
+        for(let i = 1; i < todos.length;i++){
+            console.log(`${i}: ${todos[i]}`)
+        }
+    } else if(input === "new" ){
+        const newToDo = prompt("OK, what is the new todo?");
+        todos.push(newToDo);
+        console.log(`${newToDo} added to the list`)
+    } else if(input === "delete"){
+        const index = parseInt(prompt("Ok, enter an index to delete"))
+        const deletedTarget = todos.splice(index, 1);
+        console.log(deletedTarget[0])
+        console.log(`You successfully removed ${deletedTarget[0]} from the list`)
+    }
     input =  prompt("What would you like to do?")
 }
 
